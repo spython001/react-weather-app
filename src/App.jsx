d7b03e2962cd6cc6660dacafe34b447afdca4360
spython <emmanuelegbo01@gmail.com>
2023-11-23 12:58:25 +0100

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import './App.css'
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
-
+import "./App.css";
+//import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
 function App() {
-
   return (
-    <>
-      <div>
-        <h1 className='text-2xl font-bold text-red-500'>Hello world</h1>
-        <UilReact />
-      </div>
-    </>
-  )
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
+    shadow-gray-400"></div>
+  );
 }
 
-export default App
+export default App;
