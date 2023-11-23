96901bfc308600932f3b8606bac223eb297a6a05
spython <emmanuelegbo01@gmail.com>
2023-11-23 13:57:44 +0100

    modified content of Inputs jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Inputs from "./components/Inputs";
 import TopButtons from "./components/TopButtons";
 //import UilReact from "@iconscout/react-unicons/icons/uil-react";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
     shadow-gray-400">
       <TopButtons/>
+      <Inputs/>
     </div>
   );
 }
