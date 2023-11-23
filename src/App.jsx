0895a5a4e6bef4409dfce5a0ed83bf666b0fbee1
spython <emmanuelegbo01@gmail.com>
2023-11-23 12:49:51 +0100

    added unicons package

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
 
 function App() {
 
   return (
     <>
       <div>
-        <h1 className='text-2xl font-bold text-red-500'>Hello World</h1>
+        <h1 className='text-2xl font-bold text-red-500'>Hello world</h1>
+        <UilReact />
       </div>
     </>
   )
