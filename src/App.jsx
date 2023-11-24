e78239c850f4ce1feb123160f82a3ea96870fe9c
spython <emmanuelegbo01@gmail.com>
2023-11-24 12:50:01 +0100

    added react toastify package

--- src/App.jsx
+++ src/App.jsx
@@ -23,27 +23,38 @@ function App() {
       
     };
     fetchWeather();
-  }, [query, units])
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return 'from-cyan 700 to-blue-700'
+    const threshold = units === 'metric' ? 20 : 60
+    if(weather.temp <= threshold) return 'from-cyan-700 to-blue-700'
+
+    return 'from-yellow-700 to-orange-700'
+  }
+
 
   return (
-    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
-    shadow-gray-400">
-      <TopButtons setQuery ={ setQuery }/>
-      <Inputs setQuery = { setQuery } units={ units } setUnits ={ setUnits }/>
-      
-      { 
-        weather &&
-        (
-          <div>
-            <TimeAndLocation weather ={ weather } />
-            <TemperatureAndDetails weather = { weather }/>
+    <div className="flex items-center justify-center">
+      <div className={`"mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl
+      shadow-gray-400 " ${formatBackground()}`}>
+        <TopButtons setQuery ={ setQuery }/>
+        <Inputs setQuery = { setQuery } units={ units } setUnits ={ setUnits }/>
+        
+        { 
+          weather &&
+          (
+            <div>
+              <TimeAndLocation weather ={ weather } />
+              <TemperatureAndDetails weather = { weather }/>
 
-            <Forecast title="hourly forecast" items={weather.hourly}/>
-            <Forecast title="daily forcast" items={weather.daily}/>
-          </div>
-        )
-      }
-      
+              <Forecast title="hourly forecast" items={weather.hourly}/>
+              <Forecast title="daily forcast" items={weather.daily}/>
+            </div>
+          )
+        }
+        
+      </div>
     </div>
   );
 }
